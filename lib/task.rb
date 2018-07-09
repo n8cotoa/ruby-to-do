82c005c47f2d996b4123a0fe2ce4ebeb66b12de1
Nate Cottle and Nick Brown <nate+nick@>
2018-07-09 13:58:55 -0700

    sort items fetched from database by date

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,11 @@
+
 class Task
-  attr_reader(:description, :list_id)
+  attr_reader(:description, :list_id, :due_date)
 
   def initialize(attributes)
     @description = attributes.fetch(:description)
     @list_id = attributes.fetch(:list_id)
+    @due_date = attributes.fetch(:due_date, Date.new(2018,7,9)).to_s
   end
 
   def self.all
@@ -12,16 +14,29 @@ class Task
     returned_tasks.each() do |task|
       description = task.fetch("description")
       list_id = task.fetch("list_id").to_i()
-      tasks.push(Task.new({:description => description, :list_id => list_id}))
+      due_date = task.fetch("due_date")
+      tasks.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date}))
     end
     tasks
   end
 
   def ==(another_task)
-    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
+    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id())).&(self.due_date().==(another_task.due_date()))
   end
 
   def save
-    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+    DB.exec("INSERT INTO tasks (description, list_id, due_date) VALUES ('#{@description}', #{@list_id}, '#{@due_date}');")
+  end
+
+  def self.sort
+    sorted_list = []
+    sorted = DB.exec("SELECT * FROM tasks ORDER BY due_date;")
+    sorted.each do |list|
+      description = list.fetch("description")
+      list_id = list.fetch("list_id").to_i()
+      due_date = list.fetch("due_date")
+      sorted_list.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date}))
+    end
+    sorted_list
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require 'pg'
 require 'task'
 require("spec_helper")
+require 'pry'
 
 DB = PG.connect({:dbname => 'to_do_testing'})
 
@@ -45,6 +46,19 @@ describe(Task) do
       task1 = Task.new({:description => "learn SQL", :list_id => 1})
       task2 = Task.new({:description => "learn SQL", :list_id => 1})
       expect(task1).to(eq(task2))
+    end
+  end
+
+  describe(".sort") do
+    it("sort tasks by due date") do
+      task1 = Task.new({:description => "Learn SQL", :list_id => 1, :due_date => Date.new(2018, 7, 25)})
+      task1.save()
+      task2 = Task.new({:description => "Learn Ruby", :list_id => 2, :due_date => Date.new(2018, 7, 13)})
+      task2.save()
+      task3 = Task.new({:description => "Learn Java", :list_id => 3, :due_date => Date.new(2018, 7, 29)})
+      task3.save()
+      Task.sort()
+      expect(Task.sort()).to(eq([task2, task1,task3]))
     end
   end
 end
